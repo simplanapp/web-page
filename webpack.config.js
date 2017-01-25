d1f147ebcd8c548ac15ce5f7005b2b9a12c3d9d9
oferbattar <oferbattar@gmail.com>
2017-01-25 14:33:18 +0200

    new web page

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,11 @@ module.exports = {
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
+  resolveLoder:{
+    modulesDirectories: ["web_loaders", "web_modules", "node_loaders", "node_modules"],
+    extensions: ["", ".webpack-loader.js", ".web-loader.js", ".loader.js", ".js"],
+    packageMains: ["webpackLoader", "webLoader", "loader", "main"]
+  },
   devServer: {
     historyApiFallback: true,
     contentBase: './'
