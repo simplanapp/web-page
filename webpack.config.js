a4d4b2dd8413c27de1fbecd2c214a737bf2efca5
oferbattar <oferbattar@gmail.com>
2017-01-25 14:38:37 +0200

    new web page

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,9 @@ module.exports = {
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
+  resolve: {
+   modulesDirectories: [path.resolve(__dirname, 'node_modules')]
+ },
   resolveLoder:{
     modulesDirectories: ["web_loaders", "web_modules", "node_loaders", "node_modules"],
     extensions: ["", ".webpack-loader.js", ".web-loader.js", ".loader.js", ".js"],
